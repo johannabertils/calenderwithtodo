65870105ac2756ce6489c2eea4883086fd90cef0
Johanna Bertils <johannabertils@hotmail.com>
2021-09-08 11:44:11 +0200

    printing task as li

--- src/components/Todolist/todolist.js
+++ src/components/Todolist/todolist.js
@@ -1,33 +1,44 @@
 import React, { useState, useEffect } from 'react';
+import './todolist.css';
 
 
 export default function ToDoList() {
 
-    const [taskList, setTaskList] = useState();
+
     const [showList, setShowList] = useState(false);
     const [text, setText] = useState();
 
     // set text that will show on click to the state text
-    const Text = () => <div>{text}</div>; 
+    const Text = () => <div>{text}</div>;
 
-    // when clicking on showlist btn -> show tasks
+    // when clicking on showlist btn -> if value in localstorage acitvate printList function
     function showItemsOnClick(evt, Text) {
         console.log(localStorage.data);
         evt.preventDefault();
-        setShowList(true); 
+        setShowList(true);
         console.log("hello there");
         var dataFromLocalStorage = localStorage.getItem('data');
-        if (dataFromLocalStorage === null){
+        if (dataFromLocalStorage === null) {
             setText("No tasks")
             console.log("no tasks");
         } else {
-            var getTasks = JSON.parse(dataFromLocalStorage); 
-            var dates = getTasks.map( item => item.task)
-            setText(dates)
-            // const myArrayTasks = movies2;
-            console.log("my array är" + dates);
+            printList(dataFromLocalStorage);
         }
     }
+
+    // Print tasklist
+
+    const printList = (dataFromLocalStorage) => {
+        var getTasks = JSON.parse(dataFromLocalStorage);
+        var dates = getTasks.map(item =>
+            <div className="itemList">
+                <p className="taskname"> {item.date} {item.task}
+                    <button>Mark as done</button></p></div>)
+        setText(dates)
+        // const myArrayTasks = movies2;
+        console.log("my array är" + dates);
+    }
+
 
 
     return <div>
